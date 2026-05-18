9992236e46a93d20d87389d395b474b957c5ba99
k.ananev <k.ananev@legionsecurity.ru>
2026-05-18 15:55:34 +0300

    feat(AB-1): Добавил функцию имитации battle

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { test } from '@game/core';
+import {battleResult, test} from '@game/core';
 
 console.log(test);
+console.log(battleResult);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
